12bdfa1cadd6045f3f273b46d860947f02256bd2
wtricks <wtricks.ak@gmail.com>
2024-09-03 10:52:25 +0530

    Initial commit

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+const adminController = require('../controllers/adminController');
+
+const router = express.Router();
+
+router.get('/getAllLoginLogsProAdmin', adminController.getAllLoginLogs);
+// Protect all routes after this middleware
+router.use(authController.protect);
+router.post('/sendMessageToUser',authController.restrictTo(['admin']), adminController.sendMessageToUser);
+router.post('/sendEmailToUser',authController.restrictTo(['admin']), adminController.sendEmailToUser);
+authController.verify();
+
+module.exports = router;
